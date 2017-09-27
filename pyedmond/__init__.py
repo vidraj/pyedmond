0bcfd2860068aa3eafbd323d716ebdf3c5bc375e
Han Xiao <han.xiao@aalto.fi>
2017-09-27 12:46:27 +0000

    setup.py done

---
+++ pyedmond/__init__.py
@@ -0,0 +1,1 @@
+from .core import find_minimum_branching
---
+++ setup.py
@@ -0,0 +1,29 @@
+# from distutils.core import setup, Extension
+import os
+from setuptools import setup, Extension
+
+os.environ["CC"] = "g++"
+os.environ["CXX"] = "g++"
+
+core_module = Extension(
+    'pyedmond/_core',
+    include_dirs=['/usr/include/python3.5/'],
+    libraries=['boost_python-py35', 'boost_graph'],
+    library_dirs=['/usr/lib/x86_64-linux-gnu/'],
+    extra_compile_args=['-std=c++11', '-O2', '-Wall'],
+    extra_link_args=['-Wl,--export-dynamic'],
+    sources=['pyedmond/_core.cpp']
+)
+
+setup(name='pyedmond',
+      version='0.1',
+      description='Edmond optimal branching algorithm in C++ wrapped by Python',
+      url='http://github.com/xiaohan2012/pyedmond',
+      author='Han Xiao',
+      author_email='xiaohan2012@gmail.com',
+      license='MIT',
+      packages=['pyedmond'],
+      ext_modules=[core_module],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest']
+)
